ef6796778d40c910243a0ae9c4cf15cc01f975ae
ssasmi <ssasmi@gmail.com>
2022-06-08 02:21:00 +0300

    axios

---
+++ src/axios.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+axios.defaults.baseURL = 'https://628a38525da6ddfd5d61021f.mockapi.io/userJob';
+
+axios.interceptors.request.use(function (req) {
+  const user = localStorage.getItem('user');
+
+  if (user) {
+    const { token } = JSON.parse(localStorage.getItem('user'));
+    req.headers.authorization = `Bearer ${token}`;
+    return req;
+  }
+  return req;
+});
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import React from "react";
+import { Box } from "@mui/system";
+import { Button, TextField } from "@mui/material";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const [values, setValues] = useState({ company: "", position: "" });
+  console.log(values);
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
 
-export default Dashboard
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { company, position } = values;
+    if (company && position) {
+      setValues({ company: "", position: "" });
+    }
+  };
+
+  let isLoading = true;
+
+  return (
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      noValidate
+      sx={{ display: "flex", m: "10px" }}
+    >
+      <TextField
+        margin="normal"
+        id="company"
+        label="company address"
+        name="company"
+        autoFocus
+        value={values.company}
+        onChange={handleChange}
+        rows={6}
+      />
+      <TextField
+        margin="normal"
+        name="position"
+        label="position"
+        type="position"
+        id="position"
+        value={values.position}
+        onChange={handleChange}
+        rows={6}
+      />
+
+      <Button
+        type="submit"
+        variant="contained"
+        sx={{ mt: 3, mb: 2 }}
+        disabled={!values.position}
+      >
+        {!isLoading ? "Adding New Job..." : "Add Job"}
+      </Button>
+    </Box>
+  );
+};
+
+export default Dashboard;
